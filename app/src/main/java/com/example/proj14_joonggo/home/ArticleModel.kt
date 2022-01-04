930404e18ed6472fc0c3e9bc59f3f4bb611b4d19
ksj <koreatlwls@gmail.com>
2022-01-04 21:42:15 +0900

    HomeFragment
    HomeFragment

---
+++ app/src/main/java/com/example/proj14_joonggo/home/ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.example.proj14_joonggo.home
+
+data class ArticleModel(
+    val sellerId: String,
+    val title: String,
+    val createdAt: Long,
+    val price: String,
+    val imageUrl: String
+)
